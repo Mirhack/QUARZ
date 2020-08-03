0b31dd8b2651290e1ae747a611e88cf1b3cc2d2b
Лука Горбенко <mirhack8@gmail.com>
2020-08-03 11:51:28 +0300

    Package rename

---
+++ app/src/main/java/com/mirhack/quarz/presentation/bottomSheetDialog/BottomSheetDialogViewState.kt
@@ -0,0 +1,34 @@
+package com.mirhack.quarz.presentation.bottomSheetDialog
+
+import com.mirhack.quarz.model.BarcodeContact
+
+
+data class BottomSheetDialogViewState(
+    val isPredefinedValue: Boolean = false,
+    val barcodeText: String,
+    val barcodeType: Int,
+    val wifiSsid: String,
+    val wifiPassword: String,
+    val url: String,
+    val phone: String,
+    val barcodeContact: BarcodeContact,
+    val androidVersionLessQ: Boolean
+)
+
+sealed class BottomSheetDialogViewEvent {
+    data class Init(val pdo: BottomSheetDialogPDO) : BottomSheetDialogViewEvent()
+    object ConnectWiFi : BottomSheetDialogViewEvent()
+    object CopyToClipboard : BottomSheetDialogViewEvent()
+    object OpenInBrowser : BottomSheetDialogViewEvent()
+    object AddPhoneToContacts : BottomSheetDialogViewEvent()
+    object AddContact : BottomSheetDialogViewEvent()
+    object Dial : BottomSheetDialogViewEvent()
+}
+
+sealed class BottomSheetDialogViewEffect {
+    data class ShowToast(val text: String) : BottomSheetDialogViewEffect()
+    data class OpenInBrowser(val url: String) : BottomSheetDialogViewEffect()
+    data class AddPhoneContact(val phone: String) : BottomSheetDialogViewEffect()
+    data class AddContact(val barcodeContact: BarcodeContact) : BottomSheetDialogViewEffect()
+    data class Dial(val phone: String) : BottomSheetDialogViewEffect()
+}
